40000702dd00831561ae0c99b9b2157da656f080
Alumno <Alejandro@gmail.com>
2022-07-21 20:15:54 +0200

    commit venganza

---
+++ app/src/main/java/com/example/dadoscalculadoraimc/DadosFragment.kt
@@ -0,0 +1,59 @@
+package com.example.dadoscalculadoraimc
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.dadoscalculadoraimc.databinding.FragmentDadosBinding
+import kotlin.contracts.contract
+import kotlin.random.Random
+
+
+class DadosFragment : Fragment() {
+    private var _binding:FragmentDadosBinding? = null
+    private val binding get() = _binding!!
+    private val dados =  arrayOf(
+        R.drawable.dice1, //entero
+        R.drawable.dice2,
+        R.drawable.dice3,
+        R.drawable.dice4,
+        R.drawable.dice5,
+        R.drawable.dice6)
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+
+
+
+        _binding = FragmentDadosBinding.inflate(inflater, container, false)
+        // Inflate the layout for this fragment
+        return binding.root
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.imageView.setOnClickListener {
+            val num = Random.nextInt(1,7)
+            val num2 = Random.nextInt(1,7)
+            binding.imageView.setImageResource(dados[num-1])
+            binding.imageView2.setImageResource(dados[num2-1])
+            binding.tvResultado.text = (num+num2).toString()
+        }
+
+        binding.imageView2.setOnClickListener {
+            val num = Random.nextInt(1,7)
+            val num2 = Random.nextInt(1,7)
+            binding.imageView.setImageResource(dados[num-1])
+            binding.imageView2.setImageResource(dados[num2-1])
+            binding.tvResultado.text = (num+num2).toString()
+        }
+
+    }
+
+}
+
+
---
+++ app/src/main/java/com/example/dadoscalculadoraimc/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.example.dadoscalculadoraimc
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.Navigation
+import com.example.dadoscalculadoraimc.databinding.ActivityMainBinding
+
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.bottomNavigationView.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.opDados -> {
+                    Navigation.findNavController(binding.fragmentContainerView)
+                        .navigate(R.id.dadosFragment)
+                    true
+                }
+                R.id.opCalculadora -> {
+                    Navigation.findNavController(binding.fragmentContainerView)
+                        .navigate(R.id.caculadoraFragment)
+                    true
+                }
+                R.id.opIMC -> {
+                    Navigation.findNavController(binding.fragmentContainerView)
+                        .navigate(R.id.IMCFragment)
+                    true
+                }
+                else -> false
+            }
+        }
+    }
+
+}
