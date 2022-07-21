0aef971a2f71c9b3e4325856ee9d959db2556e07
Alumno <Alejandro@gmail.com>
2022-07-21 20:23:08 +0200

    commit venganza 2

--- app/src/main/java/com/example/dadoscalculadoraimc/CaculadoraFragment.kt
+++ app/src/main/java/com/example/dadoscalculadoraimc/CaculadoraFragment.kt
@@ -5,17 +5,25 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.textclassifier.TextClassifier.NO_OP
+import com.example.dadoscalculadoraimc.databinding.FragmentCaculadoraBinding
+import com.example.dadoscalculadoraimc.databinding.FragmentDadosBinding
 
 
 class CaculadoraFragment : Fragment() {
-
-
+    private var _binding: FragmentCaculadoraBinding? = null
+    private val binding get() = _binding!!
+    private var n1 = 0
+    private var n2 = 0
+    private var op = NO_OP
+    private var SUMA = 1
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        _binding = FragmentCaculadoraBinding.inflate(inflater, container, false)
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_caculadora, container, false)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
